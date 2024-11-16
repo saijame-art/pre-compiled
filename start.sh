d2a7cd1f37e7b5bbc797881b086cf51e38b5f5f0
saijame-art <technisaiharn@gmail.com>
2024-11-16 10:15:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Define a unique worker name for each miner (set this differently on each machine)
-WORKER_NAME="Nova3i-003"  # Change this for each miner
+WORKER_NAME="VivoY83-001"  # Change this for each miner
 
 # Download the config file from GitHub
 curl -o ~/ccminer/onlineconfig.json https://raw.githubusercontent.com/saijame-art/miner/refs/heads/main/onlineconfig.json
