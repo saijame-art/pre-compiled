6241e95de3cba25b1b197471b2761fd783decccd
saijame-art <technisaiharn@gmail.com>
2025-02-07 10:09:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Define a unique worker name for each miner (set this differently on each machine)
-WORKER_NAME="HW-GR5-001"  # Change this for each miner
+WORKER_NAME="Y9-001"  # Change this for each miner
 
 # Download the config file from GitHub
 curl -o ~/ccminer/onlineconfig.json https://raw.githubusercontent.com/saijame-art/miner/refs/heads/main/onlineconfig.json
 
 # Replace "user" field in onlineconfig.json with the wallet and unique worker name
-sed -i "s/\"user\": \"[^\"]*\"/\"user\": \"RQ5XXjp6LrdZr6HCJCP6fLqVVWY8eF4MuB.$WORKER_NAME\"/" ~/ccminer/onlineconfig.json
+sed -i "s/\"user\": \"[^\"]*\"/\"user\": \"RPfk15ohBBFNKWgTC3jxaiQvnmjEWVFuMf.$WORKER_NAME\"/" ~/ccminer/onlineconfig.json
 
 # Run ccminer with the updated config file
 ~/ccminer/ccminer -c ~/ccminer/onlineconfig.json
